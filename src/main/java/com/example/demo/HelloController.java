1d2d49d1e462f2a453e47ccbe069c6e9d31d1c41
24wh1a05bx <24wh1a05bx@bvrithyderabad.edu.in>
2026-09-24 15:25:38 +0530

    Add Spring Boot demo application

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Spring Boot! 24WH1A0581-A.Ishwarya";
+    }
+}
+
+
